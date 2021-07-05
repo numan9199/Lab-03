0752849201ddc8f2ca0a77347f945406829f54dd
numan9199 <6210110176@psu.ac.th.com>
2021-07-05 11:36:05 +0700

    second

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-
+import CharacterCard from './CharacterCard';
 function App() {
-  return (
+ return (
  <div>
- Hello World
+ <CharacterCard value="h"/>
+ <CharacterCard value="i"/>
  </div>
  );
 }
+
 export default App;
