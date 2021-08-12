d825ea3d1bab33891b178371848476c7d2bf62eb
numan9199 <6210110176@psu.ac.th.com>
2021-08-12 22:18:35 +0700

    การบ้าน5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import { random } from 'lodash';
 import React from 'react';
 import './App.css';
 import WordCard from './WordCard';
 
-const word = "Programmer";
+const word = ["Programmer","Computer","Game","Lab"];
 function App() {
  return (
   <div>
-      <WordCard value={word}/>
+      <WordCard value = {word[random(0,word.length)]} />
   </div>
   );
 }
